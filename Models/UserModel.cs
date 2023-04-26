8160d6659d2be354f0e68f86d6f8ca4fc18d8adb
rudzzz <rudsolza13@gmail.com>
2023-04-25 22:49:04 -0300

    Models folder

---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginProject.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string password { get; set; }
+    }
+}
